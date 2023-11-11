0c3211cc9801a5891d19c19e40d96031d1e3b8d9
Aditya Kanu <adityakanu1st@gmail.com>
2023-11-12 00:54:28 +0530

    connect to mongoDB atlas

---
+++ src/index.js
@@ -0,0 +1,43 @@
+// require("dotenv").config({ path: "./.env" });
+
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+
+dotenv.config({
+    path: "./.env"
+});
+
+
+
+
+
+
+connectDB()
+
+
+
+
+// try catch for mongoose
+// database is always on other continent ie use await async
+
+/*
+import express from "express";
+const app = express();
+
+;(async() => {
+    try {
+        await mongoose.connect(`$(process.env.MONGODB_URL)}/${DB_NAME}`);
+        app.on("error", (error) => 
+            {
+                console.error("Error in connecting to app: ", error);
+                throw error
+            })
+            app.listen(process.env.PORT, () => {
+                console.log(`Server is running on port ${process.env.PORT}`);
+            })
+    } catch (error) {
+        console.error("Error in connecting to database: ", error);
+        throw error
+    }
+})()
+*/

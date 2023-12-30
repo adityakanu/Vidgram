42f26858a4018d2bd8a383dfa52cb77a13d92c8f
Aditya Kanu <adityakanu1st@gmail.com>
2023-12-30 13:30:33 +0530

    Add subcription model

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -183,6 +183,7 @@ const logoutUser = asyncHandler(async (req, res) => {
 
 const refreshAccessToken = asyncHandler(async (req, res) => {
     const incomingRefreshToken = req.cookies?.refreshToken || req.body?.refreshToken
+    //DOUBT: || req.header("Authorization")?.replace("Bearer ", "")
 
     if (!incomingRefreshToken) {
         throw new ApiError(401, "Unauthorized Request");
